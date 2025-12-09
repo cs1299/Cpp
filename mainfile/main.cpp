6b286050c963112cc1b5720ee9fa3dd672886fc0
r0bot <2833924511@qq.com>
2025-12-09 21:45:58 +0800

    Update main.cpp

--- mainfile/main.cpp
+++ mainfile/main.cpp
@@ -2,9 +2,19 @@
 #include "main.h"
 #include "FCFS.h"
 using namespace std;
+
+// void menu
+
 int main() {
     //cout << "Hello, World!" << endl;
     // system("pause");
-    FCFS(tasks);
+    // int choose;
+    // printf("please choose:\n1:FCFS\n");
+    // scanf("%d", &choose);
+    // if (choose == 1)
+    // {
+        FCFS(tasks);
+    // }
+
     return 0;
 }
