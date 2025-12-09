642b774b85fb97bf4b085d1a4e138437b44d382c
r0bot <2833924511@qq.com>
2025-12-09 20:04:28 +0800

    Update FCFS.cpp

--- FCFS/FCFS.cpp
+++ FCFS/FCFS.cpp
@@ -11,7 +11,7 @@
  * @param tasks 要排序的任务数组
  * @param size 任务个数
  */
-void sortWithEnterTime(TASK_s tasks[], int size)
+void SortWithEnterTime(TASK_s tasks[], int size)
 {
     TASK_s temp;
     for (int i = 0; i < size; i++)
@@ -35,7 +35,7 @@ void sortWithEnterTime(TASK_s tasks[], int size)
 void FCFS(TASK_s tasks[])
 {
     int task_num = sizeof(tasks)/sizeof(*tasks);
-    sortWithEnterTime(tasks, task_num);
+    SortWithEnterTime(tasks, task_num);
     TASKQueue ready_queue;
     TaskQueueInit(&ready_queue);
     printf("FCFS开始运行");
@@ -44,19 +44,49 @@ void FCFS(TASK_s tasks[])
     int init_time = get_time();
     while (1)
     {
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
         if (tasks[i].arrivetime<= get_time() - init_time)
         {
             //按到达时间依次将任务放进就绪队列
             last_size = ready_queue.size;
             EnterQueue(&ready_queue, &tasks[i]);
+<<<<<<< Updated upstream
+=======
+            tasks[i].status = TASK_READY;
+>>>>>>> Stashed changes
             i++;
         }
         if (ready_queue.size != 0)
         {
             //当就绪队列不为空
+<<<<<<< Updated upstream
             //运行任务
             ready_queue.firstProg->running_time
         }
+=======
+            if (ready_queue.firstProg->status == TASK_READY)
+            {
+
+                ready_queue.firstProg->status = TASK_RUNNING;
+                ready_queue.firstProg->start_time = get_time() - init_time;
+                printf("%s\t%d",ready_queue.firstProg->name,ready_queue.firstProg->start_time);
+
+            }
+            else if (ready_queue.firstProg->status == TASK_RUNNING)
+            {
+                if (ready_queue.firstProg->running_time == ready_queue.firstProg->serve_time)
+                {
+                    //如果运行时长等于设定的服务时长
+
+                }
+            }
+        }
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
     }
 }
