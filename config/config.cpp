6094d4fb35de875c642041e99dc82d6b5c64bbe8
cs1299 <477641260@qq.com>
2025-12-09 14:12:21 +0800

    Update config.cpp

--- config/config.cpp
+++ config/config.cpp
@@ -1,1 +1,19 @@
+//
+// Created by 28339 on 2025/12/9.
+//
 
+#include "config.h"
+
+TASK_s tasks[10] =
+{
+    {.name = "P1", .arrivetime = 5, .serve_time = 20},
+    {.name = "P2", .arrivetime = 17, .serve_time = 10},
+    {.name = "P3", .arrivetime = 25, .serve_time = 15},
+    {.name = "P4", .arrivetime = 33, .serve_time = 30},
+    {.name = "P5", .arrivetime = 45, .serve_time = 20},
+    {.name = "P6", .arrivetime = 50, .serve_time = 15},
+    {.name = "P7", .arrivetime = 60, .serve_time = 5},
+    {.name = "P8", .arrivetime = 68, .serve_time = 10},
+    {.name = "P9", .arrivetime = 80, .serve_time = 25},
+    {.name = "P10", .arrivetime = 100, .serve_time = 40},
+};
