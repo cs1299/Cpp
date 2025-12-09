cf84d55fd975ddac4f80a6171009099379af7ef5
r0bot <2833924511@qq.com>
2025-12-09 17:23:37 +0800

    Update FCFS.cpp

--- FCFS/FCFS.cpp
+++ FCFS/FCFS.cpp
@@ -4,7 +4,42 @@
 
 #include "FCFS.h"
 #include <stdio.h>
-void FCFS(void)
+#include "config.h"
+
+/**
+ * @brief sortWithEnterTime 按到达顺序排序
+ * @param tasks 要排序的任务数组
+ * @param size 任务个数
+ */
+void sortWithEnterTime(TASK_s tasks[], int size)
 {
-    printf("%d",1);
+    TASK_s temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (tasks[j].arrivetime > tasks[j + 1].arrivetime)
+            {
+                temp = tasks[j];
+                tasks[j] = tasks[j + 1];
+                tasks[j + 1] = temp;
+            }
+        }
+    }
 }
+
+/**
+ *
+ * @param tasks 任务数组
+ */
+void FCFS(TASK_s tasks[])
+{
+    int task_num = sizeof(tasks)/sizeof(*tasks);
+    sortWithEnterTime(tasks, task_num);
+    TASKQueue ready_queue;
+    while (1)
+    {
+
+    }
+}
+
