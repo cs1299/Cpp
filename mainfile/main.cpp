e14cc4cea3b9f53a8c75d6341f4d9be6940a5dbb
r0bot <2833924511@qq.com>
2025-12-10 19:02:37 +0800

    Update main.cpp

--- mainfile/main.cpp
+++ mainfile/main.cpp
@@ -3,10 +3,42 @@
 #include "SPF.h"
 #include <Windows.h>
 using namespace std;
+<<<<<<< Updated upstream
 int main() {
     //cout << "Hello, World!" << endl;
     SetConsoleOutputCP(CP_UTF8);//输出中文方法2
     SPF_RUN(tasks,10);
     //system("pause");
+=======
+
+int main()
+{
+    int choice;
+    printf("/**---------------MENU---------------**/");
+    printf("/* type number to choose algorithm:   */\n");
+    printf("/*  1.FCFS                            */\n");
+    printf("/*  2.SPF                             */\n");
+    printf("/*  3.RR                              */\n");
+    printf("/*  4.HRRN                            */\n");
+    printf("/**----------------------------------**/\n");
+    printf("Enter your choice:");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case a_FCFS:
+            FCFS(tasks);
+            break;
+        case a_SPF:
+
+            break;
+        case a_RR:
+            RR(tasks);
+            break;
+        case a_HRRN:
+
+            break;
+    }
+    print_summerize(tasks);
+>>>>>>> Stashed changes
     return 0;
 }
