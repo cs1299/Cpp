73028b8d044160564d10b47f65b206eb24e620ba
r0bot <2833924511@qq.com>
2025-12-10 19:05:07 +0800

    Update main.cpp

--- mainfile/main.cpp
+++ mainfile/main.cpp
@@ -2,14 +2,8 @@
 #include "main.h"
 #include "SPF.h"
 #include <Windows.h>
+
 using namespace std;
-<<<<<<< Updated upstream
-int main() {
-    //cout << "Hello, World!" << endl;
-    SetConsoleOutputCP(CP_UTF8);//输出中文方法2
-    SPF_RUN(tasks,10);
-    //system("pause");
-=======
 
 int main()
 {
@@ -39,6 +33,5 @@ int main()
             break;
     }
     print_summerize(tasks);
->>>>>>> Stashed changes
     return 0;
 }
